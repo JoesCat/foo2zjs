3dd20ba0aa05110a674458138e2dafb02b1e759f
ValdikSS <iam@valdikss.org.ru>
2026-05-12 22:15:22 +0300

    Replace FSF physical address with the license URL

--- jbig.c
+++ jbig.c
@@ -22,8 +22,8 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  along with this program; if not, see
+ *  <https://www.gnu.org/licenses/>.
  * 
  *  If you want to use this program under different license conditions,
  *  then contact the author for an arrangement.
--- oakdecode.c
+++ oakdecode.c
@@ -25,8 +25,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
  *
  * Author: Rick Richardson <rick.richardson@comcast.net>
 b*/
